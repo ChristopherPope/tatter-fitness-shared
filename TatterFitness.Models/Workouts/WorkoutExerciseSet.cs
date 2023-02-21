288c458bbbb39118af84e8c1404384946475b180
Christopher Pope <christopher@thepopes.org>
2023-02-21 07:33:45 -0500

    Add ExerciseType to Set

--- TatterFitness.Models/Workouts/WorkoutExerciseSet.cs
+++ TatterFitness.Models/Workouts/WorkoutExerciseSet.cs
@@ -17,6 +17,7 @@ namespace TatterFitness.Models.Workouts
         public double Weight { get; set; }
         public int RepCount { get; set; }
         public double Volume { get; set; }
+        public ExerciseTypes ExerciseType { get; set; }
 
         public string Key
         {
